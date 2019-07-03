57be389bcc40d2ff62a475d23c44639ac9207ace
hopesAccount <dengyuhao_123@163.com>
2019-07-03 09:30:08 +0800

    MBGD

--- Maximum Likelihood/ULA_ML.m
+++ Maximum Likelihood/ULA_ML.m
@@ -3,11 +3,11 @@ SENSOR_NUM = 8;
 MARGIN = 0.5;
 SNR = 10;
 JNR = 15;
-SNAPSHOTS = 500;
+SNAPSHOTS = 1000;
 BEAM_DIR = 20;
 
 theta_s = 25;
-theta_j = 15;
+theta_j = 10;
 amp_s = sqrt(10^(SNR/10));
 amp_j = sqrt(10^(JNR/10));
 covMat_n = eye(SENSOR_NUM);
@@ -24,24 +24,49 @@ sv_j = exp(-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)'*sind(theta_j));
 samples = sv_s*signal.' + sv_j*jammer.' + noise;
 covMat_n = covMat_n + (sv_j*jammer.')*(sv_j*jammer.')'/SNAPSHOTS;
 
-ITR = size(samples, 2);
+%------MBGD-------%
+BATCH = 100;
+BATCH_SIZE = SNAPSHOTS/100;
 theta = BEAM_DIR;
 sine = sind(theta);
 theta_hat = theta;
-for n = 1:ITR
-    sv = exp(-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)'*sine);
-    w = pinv(sqrt(sv'*pinv(covMat_n)*sv))*pinv(covMat_n)*sv;
-    dSv = (-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)').*sv;
-    d2Sv = pinv(covMat_n)*dSv*sqrt(sv'*pinv(covMat_n)*sv);
-    mu = real((dSv'*pinv(covMat_n)*sv)/(sv'*pinv(covMat_n)*sv));
-    dLf = 2*(real((d2Sv'*samples(:, n))/(w'*samples(:, n))) - mu);
-    d2Lf = 2*mu^2 - (2*d2Sv'*dSv)/(w'*sv);
-    sine = sine - pinv(d2Lf)*dLf;
+for batch = 1:BATCH
+    dir = 0;
+    for n = 1:BATCH_SIZE
+        sv = exp(-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)'*sine);
+        w = pinv(sqrt(sv'*pinv(covMat_n)*sv))*pinv(covMat_n)*sv;
+        dSv = (-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)').*sv;
+        d2Sv = pinv(covMat_n)*dSv*sqrt(sv'*pinv(covMat_n)*sv);
+        mu = real((dSv'*pinv(covMat_n)*sv)/(sv'*pinv(covMat_n)*sv));
+        dLf = 2*(real((d2Sv'*samples(:, (batch - 1)*BATCH_SIZE + n))/(w'*samples(:, (batch - 1)*BATCH_SIZE + n))) - mu);
+        d2Lf = 2*mu^2 - (2*d2Sv'*dSv)/(w'*sv);
+        dir = dir + pinv(d2Lf)*dLf;
+    end
+    sine = sine - dir/BATCH_SIZE;
     theta_hat = [theta_hat; asind(abs(sine))];
 end
 
-plot((0:ITR)', theta_hat)
+%-------SGD--------%
+% for n = 1:SNAPSHOTS
+%     sv = exp(-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)'*sine);
+%     w = pinv(sqrt(sv'*pinv(covMat_n)*sv))*pinv(covMat_n)*sv;
+%     dSv = (-1j*2*pi*MARGIN*(0:SENSOR_NUM - 1)').*sv;
+%     d2Sv = pinv(covMat_n)*dSv*sqrt(sv'*pinv(covMat_n)*sv);
+%     mu = real((dSv'*pinv(covMat_n)*sv)/(sv'*pinv(covMat_n)*sv));
+%     dLf = 2*(real((d2Sv'*samples(:, n))/(w'*samples(:, n))) - mu);
+%     d2Lf = 2*mu^2 - (2*d2Sv'*dSv)/(w'*sv);
+%     sine = sine - pinv(d2Lf)*dLf;
+%     theta_hat = [theta_hat; asind(abs(sine))];
+% end
+
+% plot((0:SNAPSHOTS)', theta_hat)
+% grid on
+% xlabel('Itrations')
+% ylabel('\theta (\circ)')
+% title('Singal + Jammer + Noise')
+
+plot((0:BATCH)', theta_hat)
 grid on
-xlabel('Itrations')
+xlabel('Batch')
 ylabel('\theta (\circ)')
 title('Singal + Jammer + Noise')

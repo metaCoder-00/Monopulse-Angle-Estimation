8ba2ed8bd973e14739fafec802b718eca78a6586
unknown <dengyuhao_123@sina.com>
2019-07-26 15:26:40 +0800

    fix

--- DE Difference Beam/DE_Monopulse.m
+++ DE Difference Beam/DE_Monopulse.m
@@ -4,34 +4,42 @@ margin = 0.5;
 BEAM_DIR = 0;
 
 NBAR = 12;
-SLL = -35;
+SLL = -25;
 
 sv_beam = exp(-1j*2*pi*margin*(0:num_array - 1)'*sind(BEAM_DIR));
-w_sum = taylorwin(num_array, NBAR, SLL);
-w_dif_coef = DE_Dif(num_subarray, num_array/2, w_sum, SLL);
+w_sum = taylorwin(num_array, NBAR, -35);
+w_dif = DE_Dif(num_subarray, num_array/2, w_sum, SLL);
 
 theta = (-85:0.1:85)';
 sum_beam = zeros(length(theta), 1);
+dif_beam = zeros(length(theta), 1);
 for n = 1:length(theta)
     sv = exp(-1j*2*pi*margin*(0:num_array - 1)'*sind(theta(n)));
     sum_beam(n) = (w_sum.*sv_beam)'*sv;
+    dif_beam(n) = (w_dif.*sv_beam)'*sv;
 end
 
-plot(theta, 20*log10(abs(sum_beam)/max(abs(sum_beam))))
+% plot(theta, 20*log10(abs(sum_beam)/max(abs(sum_beam))))
+% grid on
+% axis([-90, 90, -50, 0])
+% set(gca, 'XTICK', -90: 15: 90)
+% set(gca, 'YTICK', -50: 10: 0)
+plot(theta, 20*log10(abs(dif_beam)/max(abs(dif_beam))))
 grid on
 axis([-90, 90, -50, 0])
 set(gca, 'XTICK', -90: 15: 90)
 set(gca, 'YTICK', -50: 10: 0)
 
-function w_coef = DE_Dif(P, N, w_sum, SLL)
+function w = DE_Dif(P, N, w_sum, SLL)
     CR = 0.7;
     F = 0.5;
     f_th = 1e-4;
     MAX_ITR = 1000;
-    POP_SIZE = 8;
+    POP_SIZE = 10;
 
-    w_coef = zeros(P + N, 1);
     Pop = rand(P + N, POP_SIZE);
+    opt_chromo = zeros(P + N, 1);
+    flag = 0;
     for n = 1:MAX_ITR
         Pop = Pop(:, randperm(POP_SIZE));
         mutant = F*(Pop(:, 1) - Pop(:, 2)) + Pop(:, 3);
@@ -53,13 +61,33 @@ function w_coef = DE_Dif(P, N, w_sum, SLL)
             end
             thisCost = CostF(Pop(:, col), w_sum, SLL, P);
             newCost = CostF(newChromo, w_sum, SLL, P);
-            if newCost < thisCost
+            if newCost < f_th
+                opt_chromo = newChromo;
+                flag = 1;
+                break;
+            elseif newCost < thisCost
                 Pop(:, col) = newChromo;
             end
         end
-        
+        if flag == 1
+            break;
+        end
     end
-    
+    coef_sum = w_sum(length(w_sum)/2 + 1:end);
+    if flag ~= 1
+        cost = zeros(POP_SIZE, 1);
+        for n = 1:POP_SIZE
+            cost(n) = CostF(Pop(:, n), w_sum, SLL, P);
+        end
+        [~, index] = min(cost);
+        opt_chromo = Pop(:, index);
+    end
+    w = zeros(length(coef_sum), 1);
+    for n = P + 1:P + N
+        delta = opt_chromo(n) == (1:P)';
+        w(n - P) = coef_sum(n - P)*sum(delta.*opt_chromo(1:P));
+    end
+    w = [-flip(w); w];
 end
 
 function loss = CostF(w_coef, w_sum, SLL, P)
@@ -70,5 +98,18 @@ function loss = CostF(w_coef, w_sum, SLL, P)
         w_dif(n - P) = coef_sum(n - P)*sum(delta.*w_coef(1:P));
     end
     w_dif = [-flip(w_dif); w_dif];
-    loss = 0;
+    theta = (0:0.1:30)';
+    dif_beam = zeros(length(theta), 1);
+    for n = 1:length(theta)
+        sv = exp(-1j*2*pi*0.5*(0:length(w_dif) - 1)'*sind(theta(n)));
+        dif_beam(n) = w_dif'*sv;
+    end
+    dif_beam = 20*log10(abs(dif_beam)/max(abs(dif_beam)));
+    peaks = findpeaks(dif_beam);
+    if isempty(peaks)
+        loss = inf;
+    else
+        thisSLL = peaks(2);
+        loss = (thisSLL - SLL)^2*heaviside(thisSLL - SLL);
+    end
 end
--- Weighted Array (Talor weight & Bayliss weight)/Weighted_Array.m
+++ Weighted Array (Talor weight & Bayliss weight)/Weighted_Array.m
@@ -1,3 +1,4 @@
+clear
 num_array = 8;
 f = 10e9;
 c = 3e8;
@@ -6,23 +7,24 @@ margin = wavelength/2;
 theta_signal = 0;
 
 dir = exp(-1j*2*pi*margin*(0: num_array - 1)'*sind(theta_signal)/wavelength);
+w_s = taylorwin(num_array, 4, -35).*dir;
+w_d = baylisswin(num_array, 4, -35).*dir;
 
 theta = (-80: 0.1: 80)';
 sum_beam = zeros(length(theta), 1);
 dif_beam = zeros(length(theta), 1);
 for n = 1: length(theta)
     steerVec = exp(-1j*2*pi*margin*(0: num_array - 1)'*sind(theta(n))/wavelength);
-    weight = taylorwin(num_array, 4, -30);
-    sum_beam(n) = (weight.*dir)'*steerVec;
-    weight = baylisswin(num_array, 4, -30);
-    dif_beam(n) = (weight.*dir)'*steerVec;
+    sum_beam(n) = w_s'*steerVec;
+    dif_beam(n) = w_d'*steerVec;
 end
-
+figure
 plot(theta, 20*log10(abs(sum_beam)/max(abs(sum_beam))))
 hold on
 plot(theta, 20*log10(abs(dif_beam)/max(abs(dif_beam))))
 hold off
 legend('sum beam', 'difference beam')
+axis([-80, 80, -50, 0])
 set(gca, 'XTICK', -75: 15: 75)
 grid on
 xlabel('angle/degree')
